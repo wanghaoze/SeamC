b6b4f02e2fd7f2dce709bedbf28102b0ad0dc912
wanghaoze <1070390210@qq.com>
2020-03-23 17:35:57 +0800

    A practical version:)

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 import os
-os.system("python SeamCaver_two.py -resize -im back.jpg -out ratatouille_resize.jpg -dy 20 -dx -200 -vis")
+# os.system("python SeamCaver_two.py -resize -im back.jpg -out D://ratatouille_resize.jpg -dy 20 -dx -200 -vis")
+# from PIL import Image
+# img = Image.open("static/original.jpg")
+# print(img.size)
+import time
+print(int(time.time()))
